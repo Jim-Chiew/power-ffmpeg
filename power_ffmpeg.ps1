943a623c67bf4b34697a1f8b1634548a9ce91f12
Jim Chiew <83194348+Jim-Chiew@users.noreply.github.com>
2025-07-06 21:58:49 +0800

    Update power_ffmpeg.ps1

--- power_ffmpeg.ps1
+++ power_ffmpeg.ps1
@@ -1,6 +1,6 @@
-$encoding_prefix = '.tmp'
-$sig = 'ffmpeg'
-$sig_found_base_name = ''
+$encoding_prefix = '.tmp'  # Add to front of file name when in process of encoding
+$sig = 'ffmpeg'  # Added to end of file name within square brackets to note the video was encoded by ffmpeg.
+$sig_found_base_name = ''  # Used to hold file names that contains the $sig. used in event of continueing interupted encoding to ensure it does not re-encoded an encoded file.
 $final_msg = '`n`nReport:'
 
 foreach ($file in (ls | Sort-Object Name)){
@@ -11,6 +11,19 @@ foreach ($file in (ls | Sort-Object Name)){
   $tmp_name = "$($encoding_prefix) $($base_name) [$($sig)]$($extension)"  # Used when in process of encoding.
   $final_name = "$($base_name) [$($sig)]$($extension)"  # change tmp_name to final_name when done.
 
+  # Check if its a folder.
+  if ($file.PSIsContainer) {
+    $final_msg += "`n Skipping folder: $($base_name)"
+	continue
+  }
+  
+  # Check if its a common video file.
+  if ('.mkv', '.mp4', '.mov', '.avi' -notcontains $extension) {
+    $final_msg += "`nNot a video file: $($base_name)"
+	echo $extension
+	continue
+  }
+  
   # Check if it contains .tmp meaning was prevously in process of encoding but got interupted.
   # delete file and Skip if true.
   if ($base_name -match "^\$($encoding_prefix)") {
@@ -34,7 +47,13 @@ foreach ($file in (ls | Sort-Object Name)){
     continue
   }
 
-  ffmpeg -hwaccel cuda -i $name -c:v hevc_nvenc -map 0:v -map 0:a -map 0:s -c:a aac -c:s ass -disposition:s:0 default $tmp_name
+  # -hwaccel cuda $name -c:v hevc_nvenc -tune uhq -highbitdepth 1 -pix_fmt yuv420p10le -temporal-aq 1 -rc-lookahead 32 -spatial-aq 1 
+  # -c:v libx265 -x265-params "crf=26:profile=main10:preset=slow" -pix_fmt yuv420p10le
+  # -c:a acc
+  # -pix_fmt p010le 
+  # -cq 19-35 
+  # -disposition:s:0 default -disposition:s:0 none
+  ffmpeg -hwaccel cuda -i $name -c:v hevc_nvenc -tune uhq -disposition:s:0 default -highbitdepth 1 -pix_fmt yuv420p10le -temporal-aq 1 -rc-lookahead 32 -spatial-aq 1 -c:a copy -c:s copy -map 0 $tmp_name
   
   Rename-Item -LiteralPath "$tmp_name" -NewName $final_name
   $final_msg += "`n Finish Encoding: $($final_name)"
